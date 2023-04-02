2ee5e06fc2030c1df6ef862dc1ec1a4d0753a051
ouharri <otmanouhrri@gmail.com>
2023-04-02 20:59:28 +0000

    build: setup project

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
