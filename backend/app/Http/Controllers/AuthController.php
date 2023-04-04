dc987f05e9072ac7a7dde004b2fecf2956da6e71
ouharri <otmanouhrri@gmail.com>
2023-04-04 20:50:32 +0000

    build: setup project

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -113,7 +113,7 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => auth()->user(),
         ]);
     }
 }
