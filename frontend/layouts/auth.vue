376f7349af43d1b9792171521e67f75d92e5f780
ouharri <otmanouhrri@gmail.com>
2023-04-09 13:48:14 +0000

    build: setup project

--- frontend/layouts/auth.vue
+++ frontend/layouts/auth.vue
@@ -1,21 +1,7 @@
 <template>
-  <div>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-            crossorigin="anonymous"></script>
-    <div class="main-wrapper login-body">
-      <div class="login-wrapper">
-        <div class="container">
-          <div class="loginbox">
-            <div class="login-left">
-              <img class="img-fluid" src="/images/login.png" alt="Logo">
-            </div>
-            <nuxt/>
-          </div>
-        </div>
-      </div>
+  <div class="h-screen bg-white">
+    <div class="h-full">
+      <nuxt/>
     </div>
   </div>
 </template>
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,12 +3,12 @@ export default {
   head: {
     title: 'frontend',
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''},
-      {name: 'format-detection', content: 'telephone=no'},
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -21,9 +21,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  buildModules: ['@nuxtjs/tailwindcss'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -33,17 +31,40 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxtjs/tailwindcss',
+    // https://auth.nuxtjs.org/
+    '@nuxtjs/auth-next',
   ],
-
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://127.0.0.1:8000/api',
+    credentials: true,
+    proxyHeaders: false,
+    proxy: false,
+    retry: { retries: 3 },
+    debug: false,
+    middleware: ['csrf'],
+  },
+
+  auth: {
+    strategies: {
+      laravelJWT: {
+        provider: 'laravel/jwt',
+        url: 'http://127.0.0.1:8000',
+        token: {
+          property: 'access_token',
+          maxAge: 60 * 60,
+        },
+        refreshToken: {
+          maxAge: 20160 * 60,
+        },
+      },
+    },
   },
 
   // TailwindCSS module configuration: https://tailwindcss.nuxtjs.org/setup
   tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css'
+    cssPath: '~/assets/css/tailwind.css',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
