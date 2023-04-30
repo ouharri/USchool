180fbb8be8be7d06bcbd81782a0532b9c7ec6897
ouharri <otmanouhrri@gmail.com>
2023-04-30 01:03:21 +0000

    build: setup project

--- frontend/pages/admin/users.vue
+++ frontend/pages/admin/users.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="flex flex-col overflow-x">
+  <div class="flex flex-col overflow-x-hidden">
     <h1
       class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
     >
       Users Table :
     </h1>
 
-    <el-container class="w-full overflow-y-auto rounded-lg shadow-xs z-0 transition duration-700 ease-in-out overflow-x"
+    <el-container class="w-full overflow-x-hidden rounded-lg shadow-xs z-0 transition duration-700 ease-in-out"
                   v-loading="loading"
                   :element-loading-background="loadingBg">
       <div class="w-full flex flex-col">
-        <div class="w-full">
+        <div class="w-full overflow-x">
           <table class="w-full whitespace-no-wrap">
             <thead>
             <tr
@@ -113,7 +113,7 @@
           </table>
         </div>
         <div
-          class="w-full flex justify-between md:grid whitespace-no-wrap px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
+          class="w-full md:grid whitespace-no-wrap px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
         >
                 <span class="flex items-center col-span-3">
                   Showing 21-30 of 100
